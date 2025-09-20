500fa51e961a0e8f00b1808efff948728f490d36
jest1393-ui <jest1393@gmail.com>
2025-09-20 16:18:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ window.onload = () => {
     }
   }, 3000); // tiempo en milisegundos (3000 = 3 segundos)
 };
+
+window.onload = () => {
+  setTimeout(() => {
+    const msg = document.getElementById("mensaje");
+    if (msg) {
+      msg.style.opacity = 1;  // hace aparecer el mensaje
+    }
+  }, 3000); // 3 segundos después de cargar la página
+};
