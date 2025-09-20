d889ee5c5835ba39b3c28b8d4ff87004890671fd
jest1393-ui <jest1393@gmail.com>
2025-09-20 16:14:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+window.onload = () => {
+  setTimeout(() => {
+    const msg = document.getElementById("msg");
+    if (msg) {
+      msg.style.opacity = 1; // hace que el mensaje aparezca
+    }
+  }, 3000); // tiempo en milisegundos (3000 = 3 segundos)
+};
